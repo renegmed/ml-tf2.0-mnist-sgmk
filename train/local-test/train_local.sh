6b7448b55309c30d5b135e7bd5dc8ffeca11bf86
EC2 Default User <ec2-user@ip-172-16-123-31.ec2.internal>
2020-02-25 00:05:49 +0000

    modified to clean up train_local.sh

--- train/local-test/train_local.sh
+++ train/local-test/train_local.sh
@@ -2,17 +2,17 @@
 
 image=$1
 
-##mkdir -p test_dir/model
-##mkdir -p test_dir/output
+mkdir -p test_dir/model
+mkdir -p test_dir/output
 
-##rm test_dir/model/*
-##rm test_dir/output/*
+rm test_dir/model/*
+rm test_dir/output/*
 
 docker run -v $(pwd)/test_dir:/opt/ml \
-	-e epochs=2
+	-e epochs=2 \
 	-e SM_NUM_GPUS=0 \
-	-e SM_MODEL_DIR=/tmp/model \
-	-e SM_CHANNEL_TRAINING=/opt/ml/data \
-	-e SM_CHANNEL_VALIDATION=/opt/ml/data \
+	-e SM_MODEL_DIR=/opt/ml/model \
+	-e SM_CHANNEL_TRAINING=/opt/ml/input/data \
+	-e SM_CHANNEL_VALIDATION=/opt/ml/input/data \
 	--rm ${image} train
 
